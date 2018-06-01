8da0adb9d02425ca0fe81de0042631685c13649e
GIGABYTE <GIGABYTE@HYPERZET>
2018-06-02 02:09:03 +0900

    구조 완성 + 이벤트 핸들러 구현 진행 예정. 디자인 수정은 이벤트 구현 이후. (사실 디자인 수정은 불필요할 거 같아요)

--- VendingMachine/src/vendingFront.java
+++ VendingMachine/src/vendingFront.java
@@ -1,9 +1,16 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.*;
 
 
-public class vendingFront extends JFrame{
+public class vendingFront extends JFrame implements ActionListener{
+	//JFrame 생성
+	JFrame frame = new JFrame();
+	
 	// 최상단 패널 (money slot) 세팅
 	JPanel upper = new JPanel();
 	JButton btnMoney = new JButton("돈 투입"); 
@@ -30,18 +37,22 @@ public class vendingFront extends JFrame{
 	// 동쪽 패널 (재고 현황) 세팅
 	JPanel east = new JPanel();
 	JButton btnSoldOut = new JButton("재 고 없 음");
+	JButton btnRefund = new JButton("반 환");
 	
 	// 남쪽 패널 (디스펜서) 세팅
 	JPanel south = new JPanel();
+	JTextField txtDispenser = new JTextField(20);
+	JTextField txtChange = new JTextField(20);
 	
 	public vendingFront(){
 		super("vending machine"); 
+		// JFrame placing
+		frame.setLayout(new BorderLayout(30, 30));
 		
 		// upper panel placing
 		upper.add(txtMoney);
 		upper.add(btnMoney);
-		upper.setLayout(new FlowLayout());
-		add("North", upper);
+		frame.add("North", upper);
 		
 		// west panel placing
 		west.add(btnCoffee1);
@@ -54,7 +65,7 @@ public class vendingFront extends JFrame{
 		west.add(btnNCoffee2);
 		west.add(btnNCoffee3);
 		west.setLayout(new GridLayout(3, 3, 20, 40));
-		add("West", west);
+		frame.add("West", west);
 		
 		// center panel placing
 		center.add(txtMaking);
@@ -63,22 +74,88 @@ public class vendingFront extends JFrame{
 		txtTemperature.setEditable(false);
 		center.add(txtWater);
 		txtWater.setEditable(false);
-		add("Center", center);
+		frame.add("Center", center);
 		
 		// east panel placing
 		east.add(btnSoldOut);
-		add("East", east);
+		btnSoldOut.setEnabled(false);
+		frame.add("East", east);
 		
 		// south panel placing
+		south.add(txtDispenser);
+		south.add(txtChange);
+		txtDispenser.setEditable(false);
+		txtChange.setEditable(false);
+		frame.add("South", south);
 		
-		setSize(800, 500);
-		setVisible(true);
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 500);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void eventHandler() {
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);	
+			}	
+		});
+		
+		btnMoney.addActionListener(this);
+		btnCoffee1.addActionListener(this);
+		btnCoffee2.addActionListener(this);
+		btnCoffee3.addActionListener(this);
+		btnMCoffee1.addActionListener(this);
+		btnMCoffee2.addActionListener(this);
+		btnMCoffee3.addActionListener(this);
+		btnNCoffee1.addActionListener(this);
+		btnNCoffee2.addActionListener(this);
+		btnNCoffee3.addActionListener(this);
+		btnRefund.addActionListener(this);
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		Object obj = ae.getSource();
+		if(obj==btnMoney) btnMoney_Click();
+		if(obj==btnCoffee1) btnCoffee_Click();
+		if(obj==btnCoffee2) btnCoffee_Click();
+		if(obj==btnCoffee3) btnCoffee_Click();
+		if(obj==btnMCoffee1) btnMCoffee_Click();
+		if(obj==btnMCoffee2) btnMCoffee_Click();
+		if(obj==btnMCoffee3) btnMCoffee_Click();
+		if(obj==btnNCoffee1) btnNCoffee_Click();
+		if(obj==btnNCoffee2) btnNCoffee_Click();
+		if(obj==btnNCoffee3) btnNCoffee_Click();
+		if(obj==btnRefund) btnRefund_Click();
+		
+	}
+	
+	public void btnMoney_Click() {
+		
+	}
+	
+	public void btnCoffee_Click() {
+		
+	}
+	
+	public void btnMCoffee_Click() {
+		
+	}
+	
+	public void btnNCoffee_Click() {
+		
+	}
+	
+	public void btnRefund_Click() {
+		
 	}
 
 	public static void main(String[] args){
 		new vendingFront();
-	} 
+	}
+
+
 
 }
