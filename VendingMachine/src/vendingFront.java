4d0ce7f4a91bff20ca90b84028ab36d77a23a4de
GIGABYTE <GIGABYTE@HYPERZET>
2018-06-02 21:20:09 +0900

    제조과정 구현 완료

--- VendingMachine/src/vendingFront.java
+++ VendingMachine/src/vendingFront.java
@@ -5,7 +5,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.*;
-
+import java.awt.Robot;
 
 public class vendingFront extends JFrame implements ActionListener{
 	//JFrame 생성
@@ -15,6 +15,7 @@ public class vendingFront extends JFrame implements ActionListener{
 	JPanel upper = new JPanel();
 	JButton btnMoney = new JButton("돈 투입"); 
 	JTextField txtMoney = new JTextField(20);
+	JTextField txtError = new JTextField(20);
 	
 	// 서쪽 패널 (커피 버튼) 세팅
 	JPanel west = new JPanel();
@@ -52,24 +53,23 @@ public class vendingFront extends JFrame implements ActionListener{
 		// upper panel placing
 		upper.add(txtMoney);
 		upper.add(btnMoney);
+		upper.add(txtError); txtError.setEditable(false);
 		frame.add("North", upper);
 		
 		// west panel placing
-		west.add(btnCoffee1);
-		west.add(btnCoffee2);
-		west.add(btnCoffee3);
-		west.add(btnMCoffee1);
-		west.add(btnMCoffee2);
-		west.add(btnMCoffee3);
-		west.add(btnNCoffee1);
-		west.add(btnNCoffee2);
-		west.add(btnNCoffee3);
+		west.add(btnCoffee1); btnCoffee1.setEnabled(false);
+		west.add(btnCoffee2); btnCoffee2.setEnabled(false);
+		west.add(btnCoffee3); btnCoffee3.setEnabled(false);
+		west.add(btnMCoffee1); btnMCoffee1.setEnabled(false);
+		west.add(btnMCoffee2); btnMCoffee2.setEnabled(false);
+		west.add(btnMCoffee3); btnMCoffee3.setEnabled(false);
+		west.add(btnNCoffee1); btnNCoffee1.setEnabled(false);
+		west.add(btnNCoffee2); btnNCoffee2.setEnabled(false);
+		west.add(btnNCoffee3); btnNCoffee3.setEnabled(false);
 		west.setLayout(new GridLayout(3, 3, 20, 40));
 		frame.add("West", west);
 		
 		// center panel placing
-		center.add(txtMaking);
-		txtMaking.setEditable(false);
 		center.add(txtTemperature);
 		txtTemperature.setEditable(false);
 		center.add(txtWater);
@@ -86,12 +86,15 @@ public class vendingFront extends JFrame implements ActionListener{
 		south.add(txtChange);
 		txtDispenser.setEditable(false);
 		txtChange.setEditable(false);
+		south.setLayout(new GridLayout(1,2,40,60));
 		frame.add("South", south);
 		
 		frame.setSize(800, 500);
 		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		eventHandler();
 	}
 	
 	public void eventHandler() {
@@ -119,43 +122,94 @@ public class vendingFront extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent ae) {
 		Object obj = ae.getSource();
 		if(obj==btnMoney) btnMoney_Click();
-		if(obj==btnCoffee1) btnCoffee_Click();
-		if(obj==btnCoffee2) btnCoffee_Click();
-		if(obj==btnCoffee3) btnCoffee_Click();
-		if(obj==btnMCoffee1) btnMCoffee_Click();
-		if(obj==btnMCoffee2) btnMCoffee_Click();
-		if(obj==btnMCoffee3) btnMCoffee_Click();
-		if(obj==btnNCoffee1) btnNCoffee_Click();
-		if(obj==btnNCoffee2) btnNCoffee_Click();
-		if(obj==btnNCoffee3) btnNCoffee_Click();
+		if(obj==btnCoffee1) btnCoffee_Click(1);
+		if(obj==btnCoffee2) btnCoffee_Click(2);
+		if(obj==btnCoffee3) btnCoffee_Click(3);
+		if(obj==btnMCoffee1) btnMCoffee_Click(1);
+		if(obj==btnMCoffee2) btnMCoffee_Click(2);
+		if(obj==btnMCoffee3) btnMCoffee_Click(3);
+		if(obj==btnNCoffee1) btnNCoffee_Click(1);
+		if(obj==btnNCoffee2) btnNCoffee_Click(2);
+		if(obj==btnNCoffee3) btnNCoffee_Click(3);
 		if(obj==btnRefund) btnRefund_Click();
 		
 	}
 	
 	public void btnMoney_Click() {
-		
+		String strMoney = txtMoney.getText();
+		int money = Integer.parseInt(strMoney);
+		// acceptmoney(temp);
+		if (money < 300) {
+			txtError.setText("돈이 부족합니다!");
+			txtError.setForeground(Color.red);
+		} else {
+			txtError.setText("커피 종류를 선택하세요.");
+			txtError.setForeground(Color.blue);
+			txtMoney.setEditable(false);
+			lightBtn();
+		}
 	}
 	
-	public void btnCoffee_Click() {
-		
+	public void btnCoffee_Click(int coffeenum) {
+		JOptionPane.showMessageDialog(null, "일반커피 " + coffeenum + "의 제조를 시작합니다.");
+    	makingMsg();
+	}
+
+
+	public void btnMCoffee_Click(int coffeenum) {
+		JOptionPane.showMessageDialog(null, "믹스커피 " + coffeenum + "의 제조를 시작합니다.");
+    	makingMsg();
 	}
 	
-	public void btnMCoffee_Click() {
-		
-	}
-	
-	public void btnNCoffee_Click() {
-		
+	public void btnNCoffee_Click(int coffeenum) {
+		JOptionPane.showMessageDialog(null, "일반 차  " + coffeenum + "의 제조를 시작합니다.");
+    	makingMsg();
 	}
 	
 	public void btnRefund_Click() {
 		
 	}
+	
+	public void lightBtn() {
+		btnCoffee1.setEnabled(true);
+		btnCoffee2.setEnabled(true);
+		btnCoffee3.setEnabled(true);
+		btnMCoffee1.setEnabled(true);
+		btnMCoffee2.setEnabled(true);
+		btnMCoffee3.setEnabled(true);
+		btnNCoffee1.setEnabled(true);
+		btnNCoffee2.setEnabled(true);
+		btnNCoffee3.setEnabled(true);
+	}
+	
+	public void offBtn() {
+		btnCoffee1.setEnabled(false);
+		btnCoffee2.setEnabled(false);
+		btnCoffee3.setEnabled(false);
+		btnMCoffee1.setEnabled(false);
+		btnMCoffee2.setEnabled(false);
+		btnMCoffee3.setEnabled(false);
+		btnNCoffee1.setEnabled(false);
+		btnNCoffee2.setEnabled(false);
+		btnNCoffee3.setEnabled(false);
+	}
+	
+	public void makingMsg() {
+		this.sleep(2000);
+		txtDispenser.setText("제조 완료. 디스펜서에서 음료를 가져가세요.");
+		// 잔돈 메소드 호출
+		offBtn();
+	}
+
+	
+	public void sleep (int time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {}
+	}
 
 	public static void main(String[] args){
 		new vendingFront();
 	}
-
-
 
 }
